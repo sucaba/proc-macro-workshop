7f1153328855bfa9153a301cf66222a59e6fb7c3
sucaba <wareverbohdan@gmail.com>
2023-03-22 00:57:41 +0200

    feat(debug): 2 steps

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,43 @@
-use proc_macro::TokenStream;
+use proc_macro;
+use quote::{quote, quote_spanned};
+use syn::spanned::Spanned;
+
+use syn::{parse_macro_input, Data, DeriveInput};
+
+struct Foo<'a> {
+    name: &'a str,
+    bitmask: u8,
+}
+
+impl<'a> ::std::fmt::Debug for Foo<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Foo")
+            .field("name", &self.name)
+            .field("bitmask", &self.bitmask)
+            .finish()
+    }
+}
 
 #[proc_macro_derive(CustomDebug)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+    let Data::Struct(data) = input.data else { panic!("Expected 'struct'") };
+    let fields = data.fields.iter().map(|f| {
+        let n = f.ident.as_ref().expect("Struct should have identifier");
+        quote_spanned!( f.span() =>
+            .field(stringify!(#n), &self.#n)
+        )
+    });
+    let result: proc_macro2::TokenStream = quote!(
+        impl ::std::fmt::Debug for #name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                f.debug_struct(stringify!(#name))
+                    #( #fields )*
+                .finish()
+            }
+        }
+    );
+    // panic!("result = {result}");
+    result.into()
 }
