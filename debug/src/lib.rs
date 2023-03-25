a99df5ca2a910b6085270fecc12c5e52901d7a3b
sucaba <wareverbohdan@gmail.com>
2023-03-25 15:49:59 +0200

    feat(debug): 3 steps

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,45 +1,111 @@
 use proc_macro;
+use proc_macro2;
 use quote::{quote, quote_spanned};
 use syn::spanned::Spanned;
 
-use syn::{parse_macro_input, Data, DeriveInput, Lit, LitStr};
+use syn::{parse_macro_input, Data, DeriveInput, Expr, ExprLit, Field, Lit, LitStr};
 
-struct Foo<'a> {
-    name: &'a str,
-    bitmask: u8,
-}
+mod use_case {
+    struct Field<'a> {
+        name: &'a str,
+        bitmask: u8,
+    }
 
-impl<'a> ::std::fmt::Debug for Foo<'a> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Foo")
-            .field("name", &self.name)
-            .field("bitmask", &self.bitmask)
-            .finish()
+    impl<'a> ::std::fmt::Debug for Field<'a> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            f.write_str("Field")?;
+            f.write_str(" { ")?;
+            f.write_fmt(format_args!("name: {:?}, ", self.name))?;
+            f.write_fmt(format_args!("bitmask: 0b{:08b}", self.bitmask))?;
+            f.write_str(" }")
+        }
     }
 }
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = input.ident;
     let name_str = LitStr::new(&name.to_string(), name.span());
     let Data::Struct(data) = input.data else { panic!("Expected 'struct'") };
-    let fields = data.fields.iter().map(|f| {
-        let n = f.ident.as_ref().expect("Struct should have identifier");
-        let n_str = LitStr::new(&n.to_string(), n.span());
-        quote_spanned!( f.span() =>
-            .field(#n_str, &self.#n)
-        )
-    });
+    let mut fields = Vec::new();
+    let mut fields_iter = data.fields.iter().peekable();
+
+    while let Some(f) = fields_iter.next() {
+        let info = DebugFieldInfo(f);
+        let n = info.name();
+        let n_str = info.name_str();
+        let mut code;
+        if let Some(attr) = info.debug_attr().expect("unable to parse debug attr") {
+            let fmt = attr.format;
+            code = quote_spanned!( f.span() =>
+                f.write_fmt(format_args!(concat!(#n_str, ": ", #fmt), self.#n))?;
+            );
+        } else {
+            let fmt = "{:?}";
+            code = quote_spanned!( f.span() =>
+                f.write_fmt(format_args!(concat!(#n_str, ": ", #fmt), self.#n))?;
+            );
+        }
+
+        let needs_comma = fields_iter.peek().is_some();
+        if needs_comma {
+            code = quote!(
+                #code;
+                f.write_str(", ")?;
+            );
+        }
+        fields.push(code);
+    }
+
     let result: proc_macro2::TokenStream = quote!(
         impl ::std::fmt::Debug for #name {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                f.debug_struct(#name_str)
-                    #( #fields )*
-                .finish()
+                f.write_str(#name_str)?;
+                f.write_str(" { ")?;
+                #( #fields )*
+                f.write_str(" }")
             }
         }
     );
     // panic!("result = {result}");
     result.into()
+}
+
+#[derive(Debug)]
+struct DebugFieldAttr {
+    format: LitStr,
+}
+
+struct DebugFieldInfo<'a>(&'a Field);
+
+impl<'a> DebugFieldInfo<'a> {
+    pub fn name(&self) -> &proc_macro2::Ident {
+        self.0
+            .ident
+            .as_ref()
+            .expect("Field should have an identifier")
+    }
+
+    pub fn name_str(&self) -> LitStr {
+        let n = self.name();
+        LitStr::new(&n.to_string(), n.span())
+    }
+
+    pub fn debug_attr(&self) -> syn::Result<Option<DebugFieldAttr>> {
+        let f: &Field = self.0;
+
+        let name_value = f
+            .attrs
+            .iter()
+            .filter_map(|attr| attr.meta.require_name_value().ok())
+            .find(|name_value| name_value.path.is_ident("debug"));
+        let Some(name_value) = name_value else { return Ok(None); };
+
+        let Expr::Lit(ExprLit { lit: Lit::Str(lit), .. }) = &name_value.value else { return Ok(None) };
+
+        return Ok(Some(DebugFieldAttr {
+            format: lit.clone(),
+        }));
+    }
 }
